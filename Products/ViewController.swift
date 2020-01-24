015e0fa8ceb892feb2cd12b9cb3cbbffc3ac58a9
otetLopez <otet.tud@gmail.com>
2020-01-24 17:30:49 -0500

    Initial Commit

---
+++ Products/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Products
+//
+//  Created by otet_tud on 1/24/20.
+//  Copyright © 2020 otet_tud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
